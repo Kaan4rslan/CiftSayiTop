1f7706248e955afa57762da13f5e454702b53645
Kaan4rslan <155268464+Kaan4rslan@users.noreply.github.com>
2024-01-11 20:20:12 +0300

    Add files via upload

---
+++ 7.soru9bkaan/7.soru9bkaan/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.soru9bkaan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int toplamsayi = 0;
+            Console.WriteLine("bir sayı gir");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            
+
+            for (int i = 0; i < sayi; i+=2)
+            {
+                toplamsayi = toplamsayi + i;
+            }
+
+            Console.WriteLine("çift sayıların toplamı: " + toplamsayi);
+            Console.Read();
+
+        }
+    }
+}
